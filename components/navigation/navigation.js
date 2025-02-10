7837070fd0fc03f51bfe8f77b94f5307a94a365f
Mariusz Gitara <gitaramariusz9@gmail.com>
2025-02-10 22:35:59 +0100

    navLayout

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -2,9 +2,45 @@ import Link from 'next/link';
 
 const Navigation = () => {
   return (
-    <div>
-
-    </div>
+    <nav className='bg-white border-gray-200 dark:bg-gray-200'>
+      <div>
+        <Link href="/">
+          RamenShop
+        </Link>
+        <ul>
+          <li>
+            <Link href="/menu">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link href="/employee-panel">
+              Panel Pracownika
+            </Link>
+          </li>
+          <li>
+            <Link href="/your-order">
+              Twoje zamówienie
+            </Link>
+          </li>
+          <li>
+            <Link href="/cart">
+              Koszyk
+            </Link>
+          </li>
+          <li>
+            <Link href="/login">
+              Zaloguj
+            </Link>
+          </li>
+          <li>
+            <Link href="/logout">
+              Wyloguj
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
