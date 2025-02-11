2ebf5317fee12c758686efba6b8b8532372ea86f
Adam Wojcik <wojcikadam.03@gmail.com>
2025-02-11 22:50:19 +0100

    button panel main page v2

--- components/mainPageButtonPanel/buttonPanel.js
+++ components/mainPageButtonPanel/buttonPanel.js
@@ -8,14 +8,12 @@ const MainButtonPanel = () => {
 
     return (
         <section className="relative w-full max-w-3xl mx-auto mb-10">
-            <Link href="/menu"
-                  className="inline-block bg-orange-600 hover:bg-orange-700 text-white font-bold py-3 px-8 rounded-full text-lg md:text-xl transition-colors duration-200 mb-4">
+            <Link href="/menu" className="inline-block bg-orange-600 hover:bg-orange-700 text-white font-bold py-3 px-8 rounded-full text-lg md:text-xl transition-colors duration-200 mb-4">
                 Zobacz Menu
             </Link>
             <br/>
             {!session && (
-                <Link href="/login"
-                      className="inline-block bg-gray-800 hover:bg-gray-900 text-white font-bold py-3 px-8 rounded-full text-lg md:text-xl transition-colors duration-200">
+                <Link href="/login" className="inline-block bg-gray-800 hover:bg-gray-900 text-white font-bold py-3 px-8 rounded-full text-lg md:text-xl transition-colors duration-200">
                     Zaloguj się
                 </Link>
             )}
