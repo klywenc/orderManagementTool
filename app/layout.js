ef0eb4c1f77259736d7754dc27ecd75390b82847
Adam Wojcik <wojcikadam.03@gmail.com>
2025-02-11 01:18:15 +0100

    layoutv2

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import "./globals.css";
 import Navigation from "@/components/navigation/navigation";
 import AuthProvider from "@/components/providers/AuthProvider";
 
-
 export const metadata = {
     title: 'Megumi Ramen',
     description: 'Experience the rich flavors of authentic ramen and Asian cuisine, crafted with passion and tradition.',
