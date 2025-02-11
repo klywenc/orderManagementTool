ad1a5ebf353a62836a4d06eacd13d9835840cac4
Mariusz Gitara <gitaramariusz9@gmail.com>
2025-02-11 01:09:14 +0100

    dodanie do layoutu appProvider'a

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-'use client'
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "@/components/navigation/navigation";
+import AuthProvider from "@/components/providers/AuthProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        
+        <AuthProvider>{children}</AuthProvider>
         <Navigation></Navigation>
       </body>
     </html>
