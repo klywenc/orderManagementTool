09967a46edfc53ccc237822e45a9b395ccc34ada
Adam Wojcik <wojcikadam.03@gmail.com>
2025-02-12 20:49:31 +0100

    style update

--- app/unauthorized/page.js
+++ app/unauthorized/page.js
@@ -15,7 +15,7 @@ const UnauthorizedPage = () => {
   }, [router]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <main className="flex flex-col items-center justify-center pt-12 pb-10">
       <div className="bg-white p-8 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold text-red-600 mb-4">Brak uprawnień</h1>
         <p className="text-gray-600 mb-4">
@@ -23,7 +23,7 @@ const UnauthorizedPage = () => {
         </p>
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-500 mx-auto"></div>
       </div>
-    </div>
+    </main>
   );
 };
 
